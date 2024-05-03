d66b4a9a770f49341663f64a268bdfdd9e6ed9d4
Francisco_KevinNico_BSIT32E2 <161100540+Francisco-KevinNico-BSTI32E2@users.noreply.github.com>
2024-05-03 17:17:23 +0800

    Change Codes 2 Errors

--- ToDoListStrider/Models/ToDoItemViewModel.cs
+++ ToDoListStrider/Models/ToDoItemViewModel.cs
@@ -2,5 +2,11 @@
 {
     public class ToDoItemViewModel
     {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public ToDoItemViewModel()
+        {
+        }
     }
 }
